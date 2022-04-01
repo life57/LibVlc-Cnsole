a34708189aeec33e89dee871d98a81fadc0374e4
life57 <42088083+life57@users.noreply.github.com>
2022-04-01 19:33:46 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stream2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Core.Initialize();
+
+            var libvlc = new LibVLC(enableDebugLogs: true);
+            var mediaplayer = new MediaPlayer(libvlc);
+
+            var media = new Media(libvlc, new Uri("https://www.youtube.com/watch?v=aqz-KE-bpKQ"));
+            
+            await media.Parse(MediaParseOptions.ParseNetwork);
+            mediaplayer.Play(media.SubItems.First());
+
+            Console.ReadKey();
+        }
+    }
+}
